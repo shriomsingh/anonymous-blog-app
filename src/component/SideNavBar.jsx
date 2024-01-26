3cf9ebd45f23f64cb5b4bfaf784980b4d15ea6c2
shriomsingh <13706240+shriomsingh@users.noreply.github.com>
2024-01-26 14:19:50 +0530

    SideNavBar Modify
    git commit -m "Admin Change"
    qa!

--- src/component/SideNavBar.jsx
+++ src/component/SideNavBar.jsx
@@ -2,7 +2,7 @@ import { PlusCircle } from "lucide-react";
 
 export function SideNavBar(){
     return(
-        <div className="grid grid-cols-2 gap-2">
+        <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col items-center justify-start absolute w-72 h-1/2 inset-x-52 inset-y-36">
             <div className="flex flex-col items-center justify-center text-white gap-5">
                 <button className="flex flex-row items-center justify-start pl-6 w-64 h-14 rounded-lg bg-[#404040] hover:bg-[#4a4949]">All Post</button>
